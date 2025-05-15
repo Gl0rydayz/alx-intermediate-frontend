a6b2ed9d615924954965120eb407b94672194b9f
Oussama IOUIRY <121458608+Gl0rydayz@users.noreply.github.com>
2025-05-15 17:17:12 +0100

    fix issues

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./*.html", // Scan HTML files in the root directory
+    "./src/**/*.{html,js}", // Keep scanning HTML and JS files in src
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-} 
+};
